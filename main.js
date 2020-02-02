3fe0581911d25db34a9fc40242a2e37409ae9b1e
Corbin March <marchcorbin@gmail.com>
2020-02-02 11:13:53 -0700

    Add JS functionality

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 var close = document.querySelector(".oscarButton")
 var welcome = document.querySelector(".welcome")
-
-close.addEventListener("click", closeWindow);
+var walletButton = document.querySelector(".walletButton")
+var clipboardButton = document.querySelector(".clipboardButton")
 
 function closeWindow(){
   welcome.classList.add("hidden");
 }
+
+function hidePage(){
+  var visible = document.querySelector(".visible");
+  visible.classList.add("hidden")
+  visible.classList.remove("visible")
+}
+function showTransactions(){
+  hidePage();
+  var transactionsPage = document.querySelector("#transactionsPage")
+  transactionsPage.classList.remove("hidden")
+  transactionsPage.classList.add("visible")
+}
+function showWelcome(){
+  hidePage()
+  var welcomePage = document.querySelector("#welcomePage")
+  welcomePage.classList.remove("hidden")
+  welcomePage.classList.add("visible")
+}
+
+
+walletButton.addEventListener("click", showTransactions);
+close.addEventListener("click", closeWindow);
+clipboardButton.addEventListener("click", showWelcome)
